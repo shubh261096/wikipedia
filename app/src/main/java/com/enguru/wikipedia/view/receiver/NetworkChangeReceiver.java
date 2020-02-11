91eb3fa43fd513595de1116e47ebc4da75b52d8c
ssh-keygen <shubham.agrawal@pb.com>
2020-02-11 12:19:28 +0530

    Refactoring of code

--- app/src/main/java/com/enguru/wikipedia/view/receiver/NetworkChangeReceiver.java
+++ app/src/main/java/com/enguru/wikipedia/view/receiver/NetworkChangeReceiver.java
@@ -9,20 +9,14 @@ import androidx.lifecycle.MutableLiveData;
 import com.enguru.wikipedia.utils.CommonUtils;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
-    boolean status;
     public static MutableLiveData<Boolean> networkChange = new MutableLiveData<>();
 
     public NetworkChangeReceiver() {
     }
 
-    public void setStatus(Boolean status) {
-        networkChange.postValue(status);
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        status = CommonUtils.getConnectivityStatus(context);
-        setStatus(status);
+        networkChange.postValue(CommonUtils.getConnectivityStatus(context));
     }
 
 }
